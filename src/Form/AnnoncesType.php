9590a655b1b1023cc66378a39e03f2e22d69a38d
Kuma667 <contact@cyrilrahmoun.fr>
2020-06-08 14:23:09 +0200

    Merge des Entity / Vues / Controllers et corrections des bugs

---
+++ src/Form/AnnoncesType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonces;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnoncesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('prix')
+            ->add('ville')
+            ->add('categorie')
+            ->add('photo')
+            ->add('premium')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonces::class,
+        ]);
+    }
+}
